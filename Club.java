a38f34293239c8adeacd527b8d86ea4a8252867c
Rushane Chryzl Divinagracia <chryzlrushane@gmailcom>
2026-03-17 00:54:37 -0400

    fsf

--- Club.java
+++ Club.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * Store details of club memberships.
@@ -34,5 +35,35 @@ public class Club
     public int numberOfMembers()
     {
         return members.size();
+    }
+    public int joinedInMonth(int month){
+        if (month <1 || month > 12){
+            System.out.println("Error: Month must be between 1 and 12. Provided:" + month);
+            return 0;
+        }
+        int count = 0;
+        for (Membership member: members){
+            if(member.getMonth() == month) {
+                count++;
+            }
+        }
+        return count;
+        }
+       public ArrayList<Membership> purge(int month, int year) {
+            ArrayList<Membership> purgedMembers = new ArrayList<>();
+        if(month <1 || month > 12){
+            System.out.println("Error: Month must be between 1 and 12. Provided:" +month);
+            return purgedMembers;
+        }
+        Iterator<Membership> it = members.iterator();
+        while(it.hasNext()){
+            Membership member = it.next();
+            if(member.getMonth() == month && member.getYear() == year){
+                purgedMembers.add(member);
+                it.remove();
+            }
+        }
+        return purgedMembers;
+        
     }
 }
